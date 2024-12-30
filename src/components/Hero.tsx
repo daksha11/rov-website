666dc055669673c9f436bb56c9b20ea9b8e35c35
Daksha Rongala <rongala.daksha11@gmail.com>
2024-12-30 12:41:17 +0530

    Deploy

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,27 +6,29 @@ export function HeroScrollDemo() {
   return (
     <div className="flex flex-col overflow-hidden"
     style={{marginBottom: "0"}}>
-      <ContainerScroll
-        titleComponent={
-          <>
-            <h1 className="text-6xl text-black dark:text-white">
-              The Ultimate Studio Experience <br />
-              <span className="text-2xl md:text-[3rem] font-bold mt-1 leading-none">
-                Where Hits Matter
-              </span>
-            </h1>
-          </>
-        }
-      >
-        <Image
-          src="/image0.jpg"
-          alt="hero"
-          height={720}
-          width={1400}
-          className="mx-auto rounded-2xl object-cover h-full object-left-top"
-          draggable={false}
-        />
-      </ContainerScroll>
+    <ContainerScroll
+      titleComponent={
+        <>
+          <h1 className="text-6xl text-black dark:text-white">
+            The Ultimate Studio Experience <br />
+            <span className="text-2xl md:text-[3rem] font-bold mt-1 leading-none">
+              Where Hits Matter
+            </span>
+          </h1>
+        </>
+      }
+      backgroundImage=""  // Add this line
+    >
+      <Image
+        src="/image0.jpg"
+        alt="hero"
+        height={720}
+        width={1400}
+        className="mx-auto rounded-2xl object-cover h-full object-left-top"
+        draggable={false}
+      />
+    </ContainerScroll>
+
 
       {/* Bright white line */}
       {/*<div className="w-full h-[2px] bg-white"></div>*/}
