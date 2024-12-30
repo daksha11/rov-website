fadad21684d37065ed04b2903790a7e4a04a501b
Daksha Rongala <rongala.daksha11@gmail.com>
2024-12-30 13:21:01 +0530

    Deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // Disable ESLint checks during the build (for all files)
+    ignoreDuringBuilds: true,
+  },
+  reactStrictMode: true, // Example of another Next.js config option
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'example.com', // Domain from which images will be loaded
+        pathname: '/path/to/images/**', // Optional: match specific path patterns
+      },
+    ],
+  },
 };
 
 export default nextConfig;
